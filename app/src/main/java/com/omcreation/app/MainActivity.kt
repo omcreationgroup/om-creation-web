d9aff86f1768e2475f02b2ff6b1dee97e94219c5
omcreationgroup <nsgabani@gmail.com>
2026-03-26 20:59:22 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/omcreation/app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.omcreation.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.omcreationn.in")
+        setContentView(webView)
+    }
+}
